cb0b67c8cd8a849c1426b5488a110b58c0df8859
XhmikosR <xhmikosr@gmail.com>
2026-02-27 18:12:18 +0200

    Increase coverage

--- test.js
+++ test.js
@@ -52,3 +52,28 @@ test('return empty array if non-valid file is supplied', async t => {
 test('throw on wrong input', async t => {
 	await t.throwsAsync(decompressUnzip()('foo'), undefined, 'Expected a Buffer, got string');
 });
+
+test('handle empty zip file', async t => {
+	const buf = await fs.readFile(path.join(__dirname, 'fixtures', 'empty.zip'));
+	const files = await decompressUnzip()(buf);
+
+	t.is(files.length, 0);
+});
+
+test('handle directory with mode 0', async t => {
+	const buf = await fs.readFile(path.join(__dirname, 'fixtures', 'dir-mode0.zip'));
+	const files = await decompressUnzip()(buf);
+
+	t.is(files.length, 1);
+	t.is(files[0].type, 'directory');
+	t.is(files[0].mode, 493); // Should be set to 493 (lines 36-37)
+});
+
+test('handle corrupted zip', async t => {
+	// ZIP file with DEFLATE compression but invalid compressed data
+	// This attempts to trigger error handling in extractEntry (lines 55-56)
+	const buf = await fs.readFile(path.join(__dirname, 'fixtures', 'corrupted.zip'));
+
+	// This should trigger error handling when yauzl tries to decompress
+	await t.throwsAsync(decompressUnzip()(buf));
+});
