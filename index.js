XhmikosR/decompress-unzip

commit b25e15b99fa82e078043bd217c8353a63dc8bfde

Drop Node.js < 20 support, update packages, switch to mime instead of extension

index.js
@@ -32,7 +32,7 @@ const extractEntry = (entry, zip) => {
 
 	file.type = getType(entry, file.mode);
 
-	if (file.mode === 0 && file.type === 'directory') {
+	if (file.mode === 0 && file.mime === 'application/zip') {
 		file.mode = 493;
 	}
 

.github/workflows/ci.yml
@@ -19,17 +19,17 @@ jobs:
     strategy:
       fail-fast: false
       matrix:
-        node: [18, 20, 22]
+        node: [20, 22, 24]
         os: [ubuntu-latest, windows-latest]
 
     steps:
       - name: Clone repository
-        uses: actions/checkout@v4
+        uses: actions/checkout@v6
         with:
           persist-credentials: false
 
       - name: Set up Node.js
-        uses: actions/setup-node@v4
+        uses: actions/setup-node@v6
         with:
           node-version: ${{ matrix.node }}
           cache: npm

.github/workflows/codeql.yml
@@ -24,20 +24,20 @@ jobs:
 
     steps:
       - name: Clone repository
-        uses: actions/checkout@v4
+        uses: actions/checkout@v6
         with:
           persist-credentials: false
 
       - name: Initialize CodeQL
-        uses: github/codeql-action/init@v3
+        uses: github/codeql-action/init@v4
         with:
           languages: "javascript"
           queries: +security-and-quality
 
       - name: Autobuild
-        uses: github/codeql-action/autobuild@v3
+        uses: github/codeql-action/autobuild@v4
 
       - name: Perform CodeQL Analysis
-        uses: github/codeql-action/analyze@v3
+        uses: github/codeql-action/analyze@v4
         with:
           category: "/language:javascript"

.github/workflows/lint.yml
@@ -4,7 +4,7 @@ on: [push, pull_request, workflow_dispatch]
 
 env:
   FORCE_COLOR: 2
-  NODE: 22 # The Node.js version to run lint on
+  NODE: 24 # The Node.js version to run lint on
 
 jobs:
   run:
@@ -13,12 +13,12 @@ jobs:
 
     steps:
       - name: Clone repository
-        uses: actions/checkout@v4
+        uses: actions/checkout@v6
         with:
           persist-credentials: false
 
       - name: Set up Node.js
-        uses: actions/setup-node@v4
+        uses: actions/setup-node@v6
         with:
           node-version: ${{ env.NODE }}
           cache: npm

commit 3e0d3fc4f2a627768007b3d4cf2653e7ab8ab929

index.js: fix wrong file type check for zip files
Introduced in b25e15b99fa82e078043bd217c8353a63dc8bfde

index.js
@@ -32,7 +32,7 @@ const extractEntry = (entry, zip) => {
 
 	file.type = getType(entry, file.mode);
 
-	if (file.mode === 0 && file.mime === 'application/zip') {
+	if (file.mode === 0 && file.type === 'directory') {
 		file.mode = 493;
 	}
 
@@ -83,7 +83,7 @@ const decompressUnzip = () => async input => {
 	if (Buffer.isBuffer(input)) {
 		const type = await fileTypeFromBuffer(input);
 
-		if (!type || type.ext !== 'zip') {
+		if (!type || type.mime !== 'application/zip') {
 			return [];
 		}
 	}