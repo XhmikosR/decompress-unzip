7182225932b6aefaa07549028237ea58248f3335
XhmikosR <xhmikosr@gmail.com>
2026-03-04 08:20:28 +0200

    Update test.js

--- test.js
+++ test.js
@@ -66,12 +66,11 @@ test('handle directory with mode 0', async t => {
 
 	t.is(files.length, 1);
 	t.is(files[0].type, 'directory');
-	t.is(files[0].mode, 493); // Should be set to 493 (lines 36-37)
+	t.is(files[0].mode, 493);
 });
 
 test('handle corrupted zip', async t => {
 	// ZIP file with DEFLATE compression but invalid compressed data
-	// This attempts to trigger error handling in extractEntry (lines 55-56)
 	const buf = await fs.readFile(path.join(__dirname, 'fixtures', 'corrupted.zip'));
 
 	// This should trigger error handling when yauzl tries to decompress
